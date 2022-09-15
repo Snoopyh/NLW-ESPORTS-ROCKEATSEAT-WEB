17fc04a3d7f8134f12fe5c8922fa495a6ce25a1d
Snoopyh <ismaelxd500@gmail.com>
2022-09-15 18:28:00 -0300

    Aula 4 parte da web

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
    
       backgroundImage:{
         galaxy: "url('/background-galaxy.png')",
-        'nlw-gradient':'linear-gradient(89.86deg, #9572FC 20.08%, #43E7AD 80.94%, #E1D55D 20.57%)',
+        'nlw-gradient':'linear-gradient(89.86deg, #9572FC 10.08%, #43E7AD 65.94%, #E1D55D 100%)',
         'game-gradient':'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
     },
